0b2b3f5b00648f14b15662cdfe816875993e8393
thuhtinthit <thuhtinthit@gmail.com>
2026-05-29 13:21:01 +0700

    set up

---
+++ backend/src/lib/prisma.ts
@@ -0,0 +1,10 @@
+// import { PrismaClient } from "../generated/prisma/client.js";
+// import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+// import "dotenv/config";
+
+// const connectionString = process.env.DATABASE_URL || "file:./dev.db";
+
+// const adapter = new PrismaBetterSqlite3({ url: connectionString });
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
